ac291ed8be1683e02d270300791f48cd4c392370
James <jhjx92@gmail.com>
2023-05-18 23:53:17 -0400

    starting person class

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -2,4 +2,11 @@ package io.zipcoder.interfaces;
 
 public class Person {
 
+    final long id;
+    String name;
+
+    public Person(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
