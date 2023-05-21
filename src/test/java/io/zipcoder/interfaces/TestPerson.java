89907f5f5397560cfa0685993ca21a13e7ee3cb2
James <jhjx92@gmail.com>
2023-05-21 13:53:12 -0400

    finished test person

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,31 @@
 package io.zipcoder.interfaces;
 
-public class TestPerson {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class TestPerson {
+    @Test
+    public void testConstructor(){
+        // given
+        String name = "Bobafett";
+        long id = 5L;
+        // when
+        Person testPerson = new Person(id, name);
+        String actualName = testPerson.getName();
+        // then
+        Assert.assertEquals(name, actualName);
+    }
+    @Test
+    public void testSetName(){
+        // given
+        String name = "Bobafett";
+        long id = 5L;
+        String newName = "Nemo";
+        // when
+        Person testPerson = new Person(id, name);
+        testPerson.setName(newName);
+        String actualName = testPerson.getName();
+        // then
+        Assert.assertEquals(newName, actualName);
+    }
 }
