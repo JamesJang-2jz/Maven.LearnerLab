8740aee57ea051b9e2d11408d4dd370718c4c67b
James <jhjx92@gmail.com>
2023-05-21 13:46:47 -0400

    finished person class

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -9,4 +9,14 @@ public class Person {
         this.id = id;
         this.name = name;
     }
+    public long getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
 }
