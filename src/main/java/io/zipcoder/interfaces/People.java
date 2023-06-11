a22566c06ca12a88311b9212ab5f49d04f6670c4
James <jhjx92@gmail.com>
2023-06-10 22:37:24 -0400

    added findById People

--- src/main/java/io/zipcoder/interfaces/People.java
+++ src/main/java/io/zipcoder/interfaces/People.java
@@ -1,8 +1,21 @@
 package io.zipcoder.interfaces;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class People {
-    List<Person> personList;
-    
+    List<Person> personList = new ArrayList<>();
+
+    public void add(Person person){
+        personList.add(person);
+    }
+
+    public Person findById(long id){
+        for (Person p : personList) {
+            if (p.getId() == id){
+                return p;
+            }
+        }
+        return null;
+    }
 }
