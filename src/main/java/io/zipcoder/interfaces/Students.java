c48a3b02a17ea3c47def07b743e1191a235fa41b
James <jhjx92@gmail.com>
2023-06-12 01:16:59 -0400

    finish students constructor

--- src/main/java/io/zipcoder/interfaces/Students.java
+++ src/main/java/io/zipcoder/interfaces/Students.java
@@ -5,7 +5,12 @@ public class Students extends People{
     private static final Students instance = new Students();
 
     private Students(){
-        this.add();
+        Student student1 = new Student(1L, "Phoebe");
+        Student student2 = new Student(2L, "Joey");
+        Student student3 = new Student(3L, "Chandler");
+        this.add(student1);
+        this.add(student2);
+        this.add(student3);
     };
 
     public static Students getInstance(){
