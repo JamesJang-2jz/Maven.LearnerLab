ff939fd2dfae126403daab1263d3b5685f71ca18
James <jhjx92@gmail.com>
2023-06-11 21:40:24 -0400

    created students singleton

--- src/main/java/io/zipcoder/interfaces/Students.java
+++ src/main/java/io/zipcoder/interfaces/Students.java
@@ -1,4 +1,13 @@
 package io.zipcoder.interfaces;
 
-public class Students {
+public class Students extends People{
+
+    private static final Students instance = new Students();
+
+    private Students(){};
+
+    public static Students getInstance(){
+        return instance;
+    }
+
 }
