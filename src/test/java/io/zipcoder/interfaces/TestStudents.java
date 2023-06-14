08fb8a1be79b38b0fe1b3dbe2e21fd3ee567a827
James <jhjx92@gmail.com>
2023-06-13 23:29:28 -0400

    added students test

--- src/test/java/io/zipcoder/interfaces/TestStudents.java
+++ src/test/java/io/zipcoder/interfaces/TestStudents.java
@@ -1,11 +1,21 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestStudents {
     @Test
     public void test(){
-        
+        Students students = Students.getInstance();
+        Student student1 = new Student(1L, "Phoebe");
+        Student student2 = new Student(2L, "Joey");
+        Student student3 = new Student(3L, "Chandler");
+        // When
+
+        // Then
+        Assert.assertTrue(students.contains(student1));
+        Assert.assertTrue(students.contains(student2));
+        Assert.assertTrue(students.contains(student3));
     }
 
 }
