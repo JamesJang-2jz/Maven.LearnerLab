21d1a2aaeaf6409c99831ab2dc721dcbaa9c3b7c
JamesJang-2jz <jhjx92@gmail.com>
2023-06-20 23:28:29 -0400

    added instructor to instructors

--- src/main/java/io/zipcoder/interfaces/Instructors.java
+++ src/main/java/io/zipcoder/interfaces/Instructors.java
@@ -7,8 +7,10 @@ public class Instructors extends People<Instructor> {
     private Instructors(){
         Instructor instructor1 = new Instructor(1L, "Rocky");
         Instructor instructor2 = new Instructor(2L, "Diesel");
+        Instructor instructor3 = new Instructor(3L, "Charizard");
         this.add(instructor1);
         this.add(instructor2);
+        this.add(instructor3);
     }
 
     public static Instructors getInstance(){
